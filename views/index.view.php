d5f3ffd79306370f3445316d68a1508e9a2e37bf
shahd <bahershahd824@gmail.com>
2026-03-27 09:08:06 +0200

    adding 4 folder after updating them with new files iside.

--- views/index.view.php
+++ views/index.view.php
@@ -3,8 +3,8 @@
 <?php require('partials/banner.php') ?>
 
 <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 text-gray-300">
+        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?></p>
     </div>
 </main>
 
